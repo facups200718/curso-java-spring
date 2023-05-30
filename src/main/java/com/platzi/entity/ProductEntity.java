e4df16ab14b2fd5badf133d2bd791149d7a97c62
Facundo Perez Schneider <facup200718@gmail.com>
2023-05-30 12:04:35 -0300

    Clase 15

---
+++ src/main/java/com/platzi/entity/ProductEntity.java
@@ -0,0 +1,39 @@
+package com.platzi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer productId;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "id_categoria")
+    private Integer categoryId;
+
+    @Column(name = "codigo_barras")
+    private String barcode;
+
+    @Column(name = "precio_venta")
+    private Double price;
+
+    @Column(name = "cantidad_stock")
+    private Integer stock;
+
+    @Column(name = "estado")
+    private Boolean state;
+}
---
+++ src/main/java/com/platzi/entity/PurchaseEntity.java
@@ -0,0 +1,37 @@
+package com.platzi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PurchaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer purchaseId;
+
+    @Column(name = "id_cliente")
+    private String clientId;
+
+    @Column(name = "fecha")
+    private LocalDateTime dateTime;
+
+    @Column(name = "medio_pago")
+    private String paymentMethod;
+
+    @Column(name = "comentario")
+    private String observations;
+
+    @Column(name = "estado")
+    private String state;
+}
