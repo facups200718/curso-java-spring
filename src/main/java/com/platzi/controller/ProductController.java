9f2fcfd0a33fa84321665cb6d5b296d3abd9724d
Facundo Perez Schneider <facup200718@gmail.com>
2023-05-31 01:43:26 -0300

    Clase 28

--- src/main/java/com/platzi/controller/ProductController.java
+++ src/main/java/com/platzi/controller/ProductController.java
@@ -3,10 +3,11 @@ package com.platzi.controller;
 import com.platzi.dto.ProductDTO;
 import com.platzi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -15,27 +16,29 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    public List<ProductDTO> getAll() {
-        return productService.getAll();
+    public ResponseEntity<List<ProductDTO>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{productId}")
-    public Optional<ProductDTO> getProduct(@PathVariable Integer productId) {
-        return productService.getProduct(productId);
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Integer productId) {
+        return ResponseEntity.of(productService.getProduct(productId));
     }
 
     @GetMapping("/categories/{categoryId}")
-    public Optional<List<ProductDTO>> getByCategoryId(@PathVariable Integer categoryId) {
-        return productService.getByCategoryId(categoryId);
+    public ResponseEntity<ProductDTO> getByCategoryId(@PathVariable Integer categoryId) {
+        return ResponseEntity.of(productService.getProduct(categoryId));
     }
 
     @PostMapping()
-    public ProductDTO save(@RequestBody ProductDTO productDTO) {
-        return productService.save(productDTO);
+    public ResponseEntity<ProductDTO> save(@RequestBody ProductDTO productDTO) {
+        return new ResponseEntity<>(productService.save(productDTO), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{productId}")
-    public Boolean delete(@PathVariable Integer productId) {
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable Integer productId) {
+        return productService.delete(productId)
+                ? new ResponseEntity(HttpStatus.OK)
+                : new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
