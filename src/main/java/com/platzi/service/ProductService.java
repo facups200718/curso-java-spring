c9f5ea648cb3d2eefab3e7c82c056e4effbc7d1b
Facundo Perez Schneider <facup200718@gmail.com>
2023-05-30 22:23:18 -0300

    Clase 22 y 23

--- src/main/java/com/platzi/service/ProductService.java
+++ src/main/java/com/platzi/service/ProductService.java
@@ -1,12 +1,13 @@
 package com.platzi.service;
 import com.platzi.dto.ProductDTO;
+import com.platzi.entity.ProductEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
     List<ProductDTO> getAll();
-    List<ProductDTO> getByCategoryId(Integer categoryId);
+    Optional<List<ProductDTO>> getByCategoryId(Integer categoryId);
     Optional<List<ProductDTO>> getByStockAndState(Integer stock, Boolean state);
     Optional<ProductDTO> getById(Integer productId);
     ProductDTO save(ProductDTO product);
--- src/main/java/com/platzi/service/impl/ProductServiceImpl.java
+++ src/main/java/com/platzi/service/impl/ProductServiceImpl.java
@@ -1,29 +1,38 @@
 package com.platzi.service.impl;
 
+import com.platzi.dto.ProductDTO;
 import com.platzi.entity.ProductEntity;
+import com.platzi.mapper.ProductMapper;
 import com.platzi.repository.ProductRepository;
+import com.platzi.service.ProductService;
 
 import java.util.List;
 import java.util.Optional;
 
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
-    public List<ProductEntity> getAll() {
-        return (List<ProductEntity>) productRepository.findAll();
+    private ProductMapper productMapper;
+
+    public List<ProductDTO> getAll() {
+        List<ProductEntity> productEntityList = (List<ProductEntity>) productRepository.findAll();
+        return productMapper.toProductDTOS(productEntityList);
     }
-    public List<ProductEntity> getByCategoryId(Integer categoryId) {
-        return productRepository.findByCategoryIdOrderByNameAsc(categoryId);
+    public Optional<List<ProductDTO>> getByCategoryId(Integer categoryId) {
+        List<ProductEntity> productEntityList = productRepository.findByCategoryIdOrderByNameAsc(categoryId);
+        return Optional.of(productMapper.toProductDTOS(productEntityList));
     }
-    public Optional<List<ProductEntity>> getByStockAndState(Integer stock, Boolean state) {
-        return productRepository.findByStockLessThanAndState(stock, state);
+    public Optional<List<ProductDTO>> getByStockAndState(Integer stock, Boolean state) {
+        Optional<List<ProductEntity>> productEntityList = productRepository.findByStockLessThanAndState(stock, state);
+        return productEntityList.map(products -> productMapper.toProductDTOS(products));
     }
 
-    public Optional<ProductEntity> getById(Integer productId) {
-        return productRepository.findById(productId);
+    public Optional<ProductDTO> getById(Integer productId) {
+        return productRepository.findById(productId).map(product -> productMapper.toProductDTO(product));
     }
 
-    public ProductEntity save(ProductEntity productEntity) {
-        return productRepository.save(productEntity);
+    public ProductDTO save(ProductDTO productDTO) {
+        ProductEntity productEntity = productRepository.save(productMapper.toProductEntity(productDTO));
+        return productMapper.toProductDTO(productEntity);
     }
 
     public void delete(Integer productId) {
