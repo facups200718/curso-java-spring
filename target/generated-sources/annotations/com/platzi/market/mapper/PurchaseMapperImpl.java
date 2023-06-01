6f82785a1c78ded38b3d10d004d6ebb0cb00fea6
Facundo Perez Schneider <facup200718@gmail.com>
2023-06-01 00:08:21 -0300

    a ver ahora

--- target/generated-sources/annotations/com/platzi/market/mapper/PurchaseMapperImpl.java
+++ target/generated-sources/annotations/com/platzi/market/mapper/PurchaseMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-31T21:03:05-0300",
+    date = "2023-06-01T00:07:41-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
 )
 @Component
