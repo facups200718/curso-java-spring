05bb266f5f38850e96c55eb2414d3f272dacc475
Facundo Perez Schneider <facup200718@gmail.com>
2023-05-31 21:08:58 -0300

    Clase 34

--- src/main/java/com/platzi/market/config/SwaggerConfig.java
+++ src/main/java/com/platzi/market/config/SwaggerConfig.java
@@ -1,11 +1,17 @@
 package com.platzi.market.config;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
 
 @Configuration
 @EnableSwagger2
@@ -15,6 +21,23 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.platzi.market.controller"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        StringBuilder apiVersion = new StringBuilder("");
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader("pom.xml"));
+            apiVersion.append(model.getVersion());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ApiInfoBuilder()
+                .title("Platzi Market API")
+                .description("API documentation for Platzi Market")
+                .version(apiVersion.toString())
                 .build();
     }
 }
